2f9b2034e1da9f5db4f7059069e602c2c351eae7
namoong0917 <namoong0917@gmail.com>
2024-01-12 02:35:59 +0900

    header 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,4 +1,16 @@
 // export로 내보내기
 export function menu() {
-  console.log(menu);
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header_nav");
+
+  // headerToggle 있을때 실행 되도록
+  if (headerToggle) {
+    // 웹표준 aria-expended 활용
+    headerToggle.addEventListener("click", () => {
+      headerNav.classList.toggle("show");
+      headerToggle.getAttribute("aria-expended") === "true"
+        ? headerToggle.setAttribute("aria-expended", "false")
+        : headerToggle.setAttribute("aria-expended", "true");
+    });
+  }
 }
